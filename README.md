a0aec076ccf9db5f80d6a9ea2f0317484861ac79
Jorge Luciano <jorgelss912345@gmail.com>
2024-02-01 00:29:18 -0500

    create AppTheme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # widgets_app
 
 A new Flutter project.
+
+
+## New commands learning with git
+Go back to the last previous commit:
+- git checkout -- .
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+
+
+import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.orange,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.pink,
+  Colors.pinkAccent,
+  Colors.red,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({ 
+    this.selectedColor = 0
+  }): assert( selectedColor > 0, 'Selected color must be greater than 0'),
+      assert( selectedColor < colorList.length, 'Selected color must be less or equal than ${ colorList.length -1 }');
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[ selectedColor ],
+  );
+  
+}
